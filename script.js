dc92fcde0f00bfbf07b7478dc98a1f6b7bd3acab
Ioakeim <joackim.gr@gmail.com>
2025-11-19 17:29:36 +0200

    Updated the logic

--- script.js
+++ script.js
@@ -30,7 +30,12 @@ const gameBoard = (function () {
             return true;
         } else return false;
     }
-    return {getBoard, placeMarker, checkWin, isTie};
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
+    return {getBoard, placeMarker, checkWin, isTie, resetBoard};
 })();
 
 function createPlayer (name, marker) {
@@ -40,15 +45,22 @@ function createPlayer (name, marker) {
 const gameControl = (function() {
     const playerOne = createPlayer("John", "X");
     const playerTwo = createPlayer("Mark", "O");
+    let isGameOver = false;
     let activePlayer = playerOne;
-    const playRound = (index) => {
-       if (gameBoard.placeMarker(index, activePlayer.marker)) {
+    const playRound = (index) => { 
+        if (isGameOver) {
+                return;
+            }
+        if (gameBoard.placeMarker(index, activePlayer.marker)) {
+           
             if (gameBoard.checkWin()) {
-                console.log(`${activePlayer.name} won!`);
+                displayControl.setMessage(`${activePlayer.name} won!`);
+                isGameOver = true;
                 return;
             }
             if (gameBoard.isTie()) {
-                console.log("It's a tie!");
+                displayControl.setMessage(`It's a tie!`);
+                isGameOver = true;
                 return;
             }
             if (activePlayer === playerOne) {
@@ -56,8 +68,45 @@ const gameControl = (function() {
             } else {
                 activePlayer = playerOne;
             }
-            console.log(`Next Turn: ${activePlayer.name}`);
-       } 
-    };
-    return {playRound};
+            displayControl.setMessage(`Next Turn: ${activePlayer.name}`);
+       }};
+    const resetGame = () => {
+        activePlayer = playerOne;
+        isGameOver = false;
+        displayControl.setMessage(`It is ${activePlayer.name}'s turn!`);
+    } 
+    
+    return {playRound, resetGame};
 })();
+
+const displayControl = (function() {
+    const render = () => {
+        const board = gameBoard.getBoard();
+        const container = document.querySelector("#gameboard");
+        container.innerHTML = "";
+        board.forEach((cell, index) => {
+            let div = document.createElement("div");
+            div.classList.add("cell");
+            div.textContent = cell;
+            div.addEventListener("click", () => {
+                gameControl.playRound(index);
+                render();
+            })
+            container.appendChild(div);
+        });
+    }
+        const setMessage = (text) => {
+            const messageDiv = document.querySelector("#message");
+            messageDiv.textContent = text;
+        };
+    return {render, setMessage};
+})();
+
+const restartBtn = document.querySelector("#restartBtn");
+restartBtn.addEventListener("click", () => {
+    gameBoard.resetBoard();
+    gameControl.resetGame();
+    displayControl.setMessage("");
+    displayControl.render();
+})
+displayControl.render();
